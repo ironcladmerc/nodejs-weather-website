d65c966040bf10963dc83b275cdcc6f35b4eaab0
Mike Harrison <mikeha_ca@yahoo.com>
2019-10-02 22:32:05 -0400

    Added daily high and low temperature to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
       } else if (body.error) {
         callback("Unable to find coordinates", undefined);
       } else {
-        callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees C, the chance of rain is ${body.currently.precipProbability}`)
+        callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees C, the chance of rain is ${body.currently.precipProbability}. Today's high is ${body.daily.data[0].temperatureHigh}, low is ${body.daily.data[0].temperatureLow} degrees C.`)
       }
     });
 }
